e1fc3ea4ec812ad9901d2263132d2753e64a26fd
Rodrigo Castro <tec.rodrigocastro@gmail.com>
2025-07-19 15:36:11 -0300

    login route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,15 @@
+import 'package:base_clean_arch_bloc/src/app/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
+  initialLocation: '/login',
+  debugLogDiagnostics: true,
   routes: [
     GoRoute(
       path: '/',
       builder: (context, state) => const Placeholder(),
     ),
+    GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
   ],
 );
