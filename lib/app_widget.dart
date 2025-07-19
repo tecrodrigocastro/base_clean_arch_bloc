376175a64ff810f8900cd5da45be12da404a38f3
Rodrigo Castro <tec.rodrigocastro@gmail.com>
2025-07-19 15:36:30 -0300

    auth injector

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,18 +1,28 @@
 import 'package:base_clean_arch_bloc/routes.dart';
+import 'package:base_clean_arch_bloc/src/app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:base_clean_arch_bloc/src/core/DI/dependency_injector.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => injector<AuthBloc>(),
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routerConfig: router,
       ),
-      routerConfig: router,
     );
   }
 }
--- lib/src/core/DI/dependency_injector.dart
+++ lib/src/core/DI/dependency_injector.dart
@@ -1,4 +1,9 @@
+import 'package:base_clean_arch_bloc/src/app/features/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:base_clean_arch_bloc/src/app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:base_clean_arch_bloc/src/app/features/auth/domain/repositories/auth_repository_interface.dart';
+import 'package:base_clean_arch_bloc/src/app/features/auth/domain/usecases/login_usecase.dart';
 import 'package:base_clean_arch_bloc/src/app/features/auth/infrastructure/auth_interceptor.dart';
+import 'package:base_clean_arch_bloc/src/app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:base_clean_arch_bloc/src/core/cache/shared_preferences/shared_preferences_impl.dart';
 import 'package:base_clean_arch_bloc/src/core/client_http/dio/rest_client_dio_impl.dart';
 import 'package:base_clean_arch_bloc/src/core/client_http/logger/client_interceptor_logger_impl.dart';
@@ -29,4 +34,14 @@ void setupDependencyInjector({bool loggerApi = false}) {
       sharedPreferencesImpl: SharedPreferencesImpl(),
     );
   });
+
+  injector.registerLazySingleton<AuthRemoteDatasource>(() {
+    return AuthRemoteDatasource(
+      restClient: injector<RestClientDioImpl>(),
+    );
+  });
+
+  injector.registerLazySingleton<IAuthRepository>(() => AuthRepositoryImpl(authRemoteDatasource: injector<AuthRemoteDatasource>()));
+
+  injector.registerLazySingleton(() => AuthBloc(loginUsecase: LoginUsecase(authRepository: injector<IAuthRepository>())));
 }
