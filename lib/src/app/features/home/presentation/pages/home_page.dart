a4848fe815887a5f60b707c87e26a144dff74cf3
Rodrigo Castro <tecrodrigocastro@gmail.com>
2026-03-15 12:38:59 -0300

    adding logout

--- lib/src/app/features/home/presentation/pages/home_page.dart
+++ lib/src/app/features/home/presentation/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:base_clean_arch_bloc/routes.dart';
+import 'package:base_clean_arch_bloc/src/core/DI/dependency_injector.dart';
+import 'package:base_clean_arch_bloc/src/core/services/session_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,8 +8,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final sessionService = injector<SessionService>();
+              await sessionService.removeToken();
+              router.go('/login');
+            },
+            icon: const Icon(Icons.logout_sharp),
+          ),
+        ],
+      ),
+      body: const Center(
         child: Text('Home Page'),
       ),
     );
