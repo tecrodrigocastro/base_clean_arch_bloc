2888ff45c0d5c602766dd8f41e2fbd525bfb39a8
Rodrigo Castro <tecrodrigocastro@gmail.com>
2026-03-15 12:25:22 -0300

    Creating the home page and registering the route.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:base_clean_arch_bloc/src/app/features/auth/presentation/pages/login_page.dart';
+import 'package:base_clean_arch_bloc/src/app/features/home/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -11,5 +12,6 @@ final router = GoRouter(
       builder: (context, state) => const Placeholder(),
     ),
     GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
+    GoRoute(path: '/home', builder: (context, state) => const HomePage()),
   ],
 );
